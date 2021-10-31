e9c7659d5d52d0e1ac7503addd0c966c66875bc1
dns <pochti_zvezda@mail.ru>
2021-10-31 15:28:01 +0300

    домашка 7 доделала, не смотрела еще разбор субботний, только сегодня из Москвы прилетела, не успеваю немного на этой неделе)

--- src/com/company/Homework.java
+++ src/com/company/Homework.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public abstract class Homework {
 
     static int[] arr = generateRandomArray();
@@ -16,84 +18,95 @@ public abstract class Homework {
 
         //homeTask1
         int year = 2021;
-        task1(year);
+        isLeapYear(year);
 
         //homeTask2
-        int clientOs = 1;
+        int clientOs = 1; //0-IOs, 1-Android
         int phoneYear = 2021;
-        task2(clientOs, phoneYear);
+        checkPhoneYear(phoneYear);
+        checkClientOs(clientOs);
 
         //homeTask3
-        int deliveryDistance = 95;
-        task3(deliveryDistance);
+      int deliveryDistance = 95;
+      countDays(deliveryDistance);
 
         //homeTask4
         String abc = "aabccddefgghiijjkk";
-        task4(abc);
+        findDuplicates(abc);
 
         //homeTask5
         int[] numbers = {3, 2, 1, 6, 5};
-        task5(numbers);
+        numbers = reverseArray(numbers);
+        System.out.println(Arrays.toString(numbers));
 
         //homeTask6
-        task6();
+        int [] array = generateRandomArray();
+        mothMiddleSpending();
 
     }
 
 
-    public static void task1(int year) {
+    public static void isLeapYear(int year) {
         if (year % 400 == 0 || year % 100 ==0 || year % 4 == 0)
             System.out.println("Год является високосным");
         else
             System.out.println("Год не является високосным");
     }
-    public static void task2(int clientOs, int phoneYear) {
-        if (clientOs == 1 && phoneYear <= 2018)
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        if (clientOs == 1 && phoneYear > 2018)
-            System.out.println("Установите обычную версию приложения для iOS по ссылке");
-        if (clientOs == 0 && phoneYear <= 2018)
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        if (clientOs == 0 && phoneYear > 2018)
-            System.out.println("Установите обычную версию для Android по ссылке");
-    }
-    public static void task3(int deliveryDistance) {
-        if (deliveryDistance <= 20) {
+
+    public static void checkPhoneYear(int phoneYear) {
+        if (phoneYear < 2019) {
+            System.out.print("Установите облегченную версию ");}
+        else { System.out.print("Установите обычную версию ");
+        } }
+
+    public static void checkClientOs(int clientOs) {
+        if (clientOs == 0) {
+                System.out.println("для iOS");
+            } else {
+                    System.out.println("для Android"); } }
+
+
+    public static void countDays(int deliveryDistance) {
+      if (deliveryDistance <= 20) {
             System.out.println("Потребуется дней : 1");
-        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуется дней: 2");
-        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуется дней: 3");
-    } }
+      } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+           System.out.println("Потребуется дней: 2");
+      } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            System.out.println("Потребуется дней: 3"); } }
 
-    public static void task4(String abc) {
-            char[] lettres = abc.toCharArray();
+    public static void findDuplicates(String abc) {
+        char[] lettres = abc.toCharArray();
         for (int i = 0; i < abc.length(); i++) {
-            for (int j = i + 1; j < abc.length(); j++)
-                  if (lettres[i] == lettres[j]) {
-                    System.out.print(lettres[j]); break;
-                }
-                 }
-System.out.println();
-        }
+          for (int j = i + 1; j < abc.length(); j++)
+              if (lettres[i] == lettres[j]) {
+                  System.out.print(lettres[j]); return;
+                } else { System.out.print("Дубликаты не найдены"); return; } } }
+    { System.out.println(); }
 
-
-
-    public static void task5(int[] numbers) {
-        for(int i = numbers.length - 1; i >=0; i--)
-            System.out.print(numbers[i]);
+    public static int[] reverseArray(int[] numbers) {
         System.out.println();
-    }
+      int head = 0;
+      int tail = numbers.length - 1;
+      while (head<tail) {
+          int temp = numbers[head];
+          numbers[head++] = numbers[tail];
+          numbers[tail--] = temp;
+      }
+       return numbers; }
 
-    public static void task6() {
-        float all = 0f;
+
+    public static float allSum() {
+        float allSum = 0f;
         for (float i : arr) {
-            all += i;
-        }
-        float mothMiddleSpending = all / arr.length;
-        System.out.println("Средняя сумма трат за месяц составила " + mothMiddleSpending + " рублей"); }
+            allSum += i; }
+        return allSum;
+    }
+    public static float mothMiddleSpending() {
 
-}
+        float mothMiddleSpending =  allSum() /  arr.length;
+     System.out.println("Средняя сумма трат за месяц составила " + mothMiddleSpending + " рублей");
+    return mothMiddleSpending; } }
+
 
 
 
